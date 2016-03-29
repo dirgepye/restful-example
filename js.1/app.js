14b0bda8a61ffc6f985885754b47365f1866d8a9
dirgepye <s.stenhouse87@gmail.com>
2016-03-29 07:31:58 +0000

    test

--- js.1/app.js
+++ js.1/app.js
@@ -19,7 +19,7 @@ function getAddressBooks(limit, pageNum) {
             else {
                 hasNextPage = false;
             }
-
+            
             return {
                 addressBooks: addressBooks,
                 hasNextPage: hasNextPage
@@ -48,22 +48,17 @@ function getEntry(entryId) {
 
 // Functions that display things on the screen (views)
 function displayAddressBooksList(pageNum) {
-    $app.html(''); // Clear the #app div
-    $app.append('<h2>Address Books List</h2>');
-    
     getAddressBooks(5, pageNum).then(
         function(results) {
             var addressBooks = results.addressBooks;
-
-            var $template = $('#addressbookListTemplate').text();
-            
-            var displayAddressBookList = _.template($template);
-            
-            $app.append(displayAddressBookList({
-                addressBooks:addressBooks
-            }));
-
+            $app.html(''); // Clear the #app div
+            $app.append('<h2>Address Books List</h2>');
+            $app.append('<ul>');
             $app.append('<button class="prev">< Prev</button> <button class="next">Next ></button>');
+
+            addressBooks.forEach(function(ab) {
+                $app.find('ul').append('<li data-name="' + ab.name + '"data-id="' + ab.id + '">' + ab.name + '</li>');
+            });
 
             $app.find('li').on('click', function() {
                 var addressBookId = $(this).data('id');
@@ -80,10 +75,10 @@ function displayAddressBooksList(pageNum) {
                     displayAddressBooksList(pageNum + 1);
                 });
             }
-
             $app.find('.prev').on('click', function() {
                 displayAddressBooksList(pageNum - 1);
             });
+
         }
     );
 }
@@ -92,41 +87,18 @@ function displayAddressBooksList(pageNum) {
 function displayAddressBook(addressBookId, addressBookName) {
     AB_ID = addressBookId;
     AB_NAME = addressBookName;
-
-    $app.html('');
-    $app.append("<h2>" + addressBookName + "'s Address Book</h2>");
-    $app.append('<button class="add">Add</button>');
-
     getEntries(addressBookId).then(
         function(Entries) {
-            //display
-            var $template = $('#entriesListTemplate').text();
+            $app.html('');
+            
+            $app.append("<h2>" + addressBookName + "'s Address Book</h2>");
+            $app.append('<ul>');
 
-            var displayEntriesList = _.template($template);
+            Entries.forEach(function(Entry) {
+                $app.find('ul').append('<li data-name="' + Entry.firstName + '"data-id="' + Entry.id + '">' + Entry.firstName + ' ' + Entry.lastName + '</li>');
 
-            // $app.append('<ul>');
-
-            // Entries.forEach(function(Entry) {
-            //     $app.find('ul').append('<li data-name="' + Entry.firstName + '"data-id="' + Entry.id + '">' + Entry.firstName + ' ' + Entry.lastName + '</li><button data-id = '+Entry.id+' class="delEntry">Delete</button>');
-            // });
-
-            $app.append(displayEntriesList({
-                entriesList: Entries
-            }));
-
-            $app.find('.delEntry').on('click', function(evt) {
-
-                console.log($(this).data('id'));
-                //evt.preventDefault();
-
-                $.ajax({
-                    url: 'https://loopback-rest-api-demo-ziad-saab.c9.io/api/Entries/' + $(this).data('id'),
-                    method: 'DELETE',
-                }).then(function() {
-                    displayAddressBook(AB_ID, AB_NAME);
-                });
             });
-
+            
             $app.append('<button class="menu return">Return to address book menu</button>');
 
             $app.find('li').on('click', function() {
@@ -136,114 +108,41 @@ function displayAddressBook(addressBookId, addressBookName) {
                 console.log(entryId, entryName);
             });
 
-
-
-
-            //return button
-
-
             $app.find('.return').on('click', function() {
                 displayAddressBooksList(0);
-            });
-
-            //add button
-
-            $app.find('.add').on('click', function() {
-                $app.find('ul').append('<form id="addEntry">First Name:<input id="firstNameAdd" type="text" name="firstName"><br>Last Name:<input id="lastNameAdd" type="text" name="lastName"><br>Date of Birth:<input id="birthdayAdd" type="text" name="birthday"><input type="submit" name="submit"></form>');
-                //form doesn't exist until the 'click' is activated
-                $('#addEntry').on('submit', function(evt) {
-
-                    evt.preventDefault();
-
-                    var firstNameAdd = $('#firstNameAdd').val();
-                    console.log(firstNameAdd);
-
-                    var lastNameAdd = $('#lastNameAdd').val();
-                    console.log(lastNameAdd);
-
-                    var birthdayAdd = $('#birthdayAdd').val();
-                    console.log(birthdayAdd)
-
-                    $.ajax({
-                        url: 'https://loopback-rest-api-demo-ziad-saab.c9.io/api/AddressBooks/' + AB_ID + '/Entries',
-                        method: 'POST',
-                        data: {
-                            firstName: firstNameAdd,
-                            lastName: lastNameAdd,
-                            birthday: birthdayAdd,
-                        }
-                    }).then(function() {
-                        displayAddressBook(AB_ID, AB_NAME);
-                    });
-                });
             });
         }
     );
 }
 
-function displayEntry(entryId) {
+function displayEntry(entryId, entryName) {
     getEntry(entryId).then(
         function(entryId) {
             console.log(entryId);
-            // display
-
             $app.html('');
-            $app.append("<h2>Info for " + entryId.firstName + " <button class='edit'>Edit</button></h>");
+            
+            $app.append("<h2>Info for " + entryId.firstName + " <button>Edit</button></h>");
             $app.append('<table>');
 
-            //table
             $app.find('table').append("<tr><td class='type'>Name:</td><td>" + entryId.firstName + " " + entryId.lastName + "</td></tr><tr><td class='type'>Birthday:</td><td>" + entryId.birthday + "</td></tr>");
-
 
             entryId.addresses.forEach(function(address) {
                 $app.find('table').append('<tr><td class="type">Address:</td><td><strong>' + address.type + "</strong><br>" + address.line1 + "<br>" + address.line2 + "<br>" + address.city + ", " + address.state + "<br>" + address.zip + "<br>" + address.country + '</td></tr>');
             });
 
-
             entryId.emails.forEach(function(email) {
                 $app.find('table').append('<tr><td class="type">Email:</td><td><strong>' + email.type + '</strong><br>' + email.email + '</td></tr>');
             });
 
-
             entryId.phones.forEach(function(phone) {
                 $app.find('table').append('<tr><td class="type">Phone:</td><td><strong>' + phone.type + '</strong><br>' + phone.phoneNumber + '<br>' + phone.phoneType + '</td></tr>');
             });
-
-            //return button
+            
             $app.append("<button class='entry return'>Return to Address Book</button>");
-
-
+            
             $app.find('.return').on('click', function() {
+                
                 displayAddressBook(AB_ID, AB_NAME);
-            });
-
-            //edit
-            $app.find('.edit').on('click', function() {
-                //$app.append('<div class="overlay"></div>');
-                $app.find('table').append("<form id='entryForm'>Home:<input type='radio' name='home'> Work:<input type='radio' name='work'> Other:<input type='radio' name='radio'><br> First Name<input id='firstNameInput' type='text' name='firstName'><br> Last Name<input id='lastNameInput' type='text' name='lastName'><br> Date of Birth<input id='birthdayInput' type='text' name='birthday'><input type='submit' name='submit'></form>");
-
-                $('#entryForm').on('submit', function(evt) {
-                    evt.preventDefault();
-
-                    var firstName = $('#firstNameInput').val();
-                    console.log(firstName);
-
-                    var lastName = $('#lastNameInput').val();
-                    console.log(lastName);
-
-                    var birthday = $('#birthdayInput').val();
-
-                    $.ajax({
-                        url: 'https://loopback-rest-api-demo-ziad-saab.c9.io/api/Entries/' + entryId.id,
-                        method: 'PUT',
-                        data: {
-                            firstName: firstName,
-                            lastName: lastName,
-                        }
-                    }).then(function() {
-                        displayEntry(entryId.id);
-                    });
-                });
             });
         });
 }
